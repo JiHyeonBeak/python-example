21147ca5a3a7550748791b4187d665461d5753da
jh <jadehl09@gmail.com>
2024-03-28 21:45:46 +0900

    add array

--- example/mini-quiz.py
+++ example/mini-quiz.py
@@ -1,6 +1,13 @@
-print("책상 위 시계의 색상은?")
-answer = input()
-if answer == "blue" or answer == "파랑" or answer == "파란색" or answer == "파랑색":
-    print("정답입니다.")
-else:
-    print("틀렸습니다.")
+QUESTION = ["책상 위 시계의 색상은?",
+            "시계 옆 책의 이름은?",
+            "시계의 시침이 있는 숫자는?"]
+
+ANSWER = ["파랑","셜록 홈즈의 귀환","3"]
+
+for i in range(3):
+    print(QUESTION[i])
+    ans = input()
+    if ans == ANSWER[i]:
+        print("정답")
+    else:
+        print("오답")
