2f59e3cd6406e3bcd7091267a704178d9920daa9
jh <jadehl09@gmail.com>
2024-03-30 19:46:11 +0900

    add another answer

--- example/mini-quiz.py
+++ example/mini-quiz.py
@@ -2,12 +2,13 @@ QUESTION = ["책상 위 시계의 색상은?",
             "시계 옆 책의 이름은?",
             "시계의 시침이 있는 숫자는?"]
 
-ANSWER = ["파랑","셜록 홈즈의 귀환","3"]
+ANSWER1 = ["파랑","셜록 홈즈의 귀환","3"]
+ANSWER2 = ["blue","return of Sherlock Holmes","three"]
 
 for i in range(3):
     print(QUESTION[i])
     ans = input()
-    if ans == ANSWER[i]:
+    if ans == ANSWER1[i] or ans == ANSWER2[i]:
         print("정답")
     else:
         print("오답")
