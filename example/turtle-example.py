af83c20f40ab5913feb0824faf2aea505d0e6ccd
jh <jadehl09@gmail.com>
2024-04-05 22:45:04 +0900

    update turtle

--- example/turtle-example.py
+++ example/turtle-example.py
@@ -1,10 +1,11 @@
 import turtle as t
+import random as r
 
 # 화면 초기화
 t.clearscreen()
-t.setup(700, 700)
+t.setup(500, 500)
 
-colors = ["red","green","skyblue","gray"]
+colors = ["green","skyblue","gray","pink"]
 
 def settingTurtle():
     for i in range(4):
@@ -14,15 +15,26 @@ def settingTurtle():
         globals()["t{}".format(i)].color(colors[i])
 
 def draw():
-    for j in range(117):
+    flag = 0
+    count = 0
+    while True:
         for k in range(4):
+            flag = r.randrange(5,15)
+            # 홀수번호 거북이는...
             if k//2 == 1:
-                globals()["t{}".format(k)].left(3)
-                globals()["t{}".format(k)].forward(10)
+                if count//2 == 1:
+                    globals()["t{}".format(k)].right(flag)
+                else:    
+                    globals()["t{}".format(k)].left(flag)
+                globals()["t{}".format(k)].backward(15)
+            # 짝수번호 거북이는...
             else:
-                globals()["t{}".format(k)].right(3)
-                globals()["t{}".format(k)].forward(10)
-        
-
+                if count//2 == 1:
+                    globals()["t{}".format(k)].left(flag)
+                else:    
+                    globals()["t{}".format(k)].right(flag)
+                globals()["t{}".format(k)].forward(15)
+            count+=1
+    
 settingTurtle()
 draw()
