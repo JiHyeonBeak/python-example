bc9de3860243d851e26b4d8098a9b350ead15a69
jh <jadehl09@gmail.com>
2024-04-09 21:54:32 +0900

    add comment

--- example/asending-data.py
+++ example/asending-data.py
@@ -1,8 +1,15 @@
 import json
 import pandas as pd
 
+# 정렬할 데이터(txt)를 받아온다.
 data = open("data.txt","r")
 pdata = data.read()
+
+# 데이터 확인
 print(pdata)
+
+# txt파일을 json으로 변환한다.
 processingData = json.loads(pdata)
+
+# 정렬
 json.dumps(processingData,sort_keys=True, indent=4)
--- example/turtle-example.py
+++ example/turtle-example.py
@@ -1,5 +1,4 @@
 import turtle as t
-import random as r
 
 # 화면 초기화
 t.clearscreen()
@@ -7,6 +6,7 @@ t.setup(500, 500)
 
 colors = ["green","skyblue","gray","pink"]
 
+# 거북이 객체 생성
 def settingTurtle():
     for i in range(1,5):
         globals()["t{}".format(i)] = t.Turtle()
@@ -14,13 +14,13 @@ def settingTurtle():
         globals()["t{}".format(i)].turtlesize(1,1,1)
         globals()["t{}".format(i)].color(colors[-i])
 
-
-
+# draw 상위 함수
 def draw():
     count = 0
     flag = True
     setHead()
     while True:
+        # 짝수번 실행시에는 flag변수를 넘겨 거북이들이 오른쪽으로 가게한다.
         if (count%2) == 0:
           workingTurtles(flag,count)
           flag = False
@@ -29,9 +29,11 @@ def draw():
             flag = True
         count += 1
 
+# 실제 거북이들이 무늬를 그리는 함수
 def workingTurtles(flag,count):
     bufferNum = 0
     for i in range(1,5):
+        # bufferNum 각 거북이들과 거리를 두기 위한 변수
         bufferNum = i*2
         if flag == True:
             globals()["t{}".format(i)].right(20+bufferNum)
@@ -39,34 +41,15 @@ def workingTurtles(flag,count):
         else:
             globals()["t{}".format(i)].left(50+bufferNum)
             globals()["t{}".format(i)].forward(15+count)
-           
+
+
+# 거북이 머리 방향 조정 함수
+# 1번, 2번 거북이는 반대방향에서 시작합니다.         
 def setHead():
     for j in range(1,5):
         if j > 2:
             globals()["t{}".format(j)].right(160)
 
-# def draw():
-#     flag = 0
-#     count = 0
-#     while True:
-#         for k in range(4):
-#             flag = r.randrange(5,15)
-#             # 홀수번호 거북이는...
-#             if k//2 == 1:
-#                 if count//2 == 1:
-#                     globals()["t{}".format(k)].right(flag)
-#                 else:    
-#                     globals()["t{}".format(k)].left(flag)
-#                 globals()["t{}".format(k)].backward(15)
-#             # 짝수번호 거북이는...
-#             else:
-#                 globals()["t{}".format(k)].right(180)
-#                 if count//2 == 1:
-#                     globals()["t{}".format(k)].left(flag)
-#                 else:    
-#                     globals()["t{}".format(k)].right(flag)
-#                 globals()["t{}".format(k)].forward(15)
-#             count+=1
-    
+# 실행부    
 settingTurtle()
 draw()
