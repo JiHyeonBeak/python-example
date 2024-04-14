e1bbdd4f6dcd882192f42337c545de04a90b8993
jh <jadehl09@gmail.com>
2024-04-14 11:39:04 +0900

    add retrospect

--- example/mini-quiz.py
+++ example/mini-quiz.py
@@ -6,10 +6,12 @@ ANSWER1 = ["파랑","셜록 홈즈의 귀환","3"]
 ANSWER2 = ["blue","return of Sherlock Holmes","three"]
 
 score = 0
+UANSER = []
 
 for i in range(3):
     print(QUESTION[i])
     ans = input()
+    UANSER.append(ans)
     if ans == ANSWER1[i] or ans == ANSWER2[i]:
         score += 1
         print("정답")
@@ -17,3 +19,6 @@ for i in range(3):
         print("오답")
 
 print("결과 :",score)
+print("-- 당신의 답 --")
+for j in range(len(UANSER)):
+    print(UANSER[j])
